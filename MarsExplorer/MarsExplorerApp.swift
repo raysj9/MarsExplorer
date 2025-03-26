1d002f964f5e1a6832d6091263bac9b1a8ef80aa
Samuel Ray <139031051+raysj9@users.noreply.github.com>
2025-03-25 23:50:38 -0400

    Initial Commit

---
+++ MarsExplorer/MarsExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarsExplorerApp.swift
+//  MarsExplorer
+//
+//  Created by Jackson Ray on 3/25/25.
+//
+
+import SwiftUI
+
+@main
+struct MarsExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
